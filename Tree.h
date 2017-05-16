a1510df12a8470d9d928b844094ad5ee6001af88
Kim <ybk@ornl.gov>
2017-05-16 18:13:48 -0400

    Tree class works in CUDA.

--- Tree.h
+++ Tree.h
@@ -16,30 +16,32 @@ private:
 
 public:
   typedef vtkm::Vec<vtkm::Float32, 3> vec3;
-
+  VTKM_EXEC_CONT
   Tree()
   {
   }
+  //Tree(const Tree &rhs):cnt(rhs.cnt), vtx(rhs.vtx), idx(rhs.idx) {
 
-  void vtxPush();
-  void idxvtxPush();
-  void AddCell(const vtkm::UInt8 shape);
-  void AddPoint(vtkm::Float32, vtkm::Float32, vtkm::Float32);
-  void AddCellPoint(vtkm::Id idx);
-  void AddCellPoint();
-  void Create();
+  //}
+
+  VTKM_EXEC_CONT
   vtkm::UInt32 getCnt(vtkm::Id id) const {return cnt.Get(id);}
+  VTKM_EXEC_CONT
   vtkm::UInt32 getIdx(vtkm::Id id) const {return idx.Get(id);}
+  VTKM_EXEC_CONT
   vtkm::UInt32 getVtx(vtkm::Id id) const {return vtx.Get(id);}
 
+  VTKM_CONT
   void SetCnt(vtkm::cont::ArrayHandle<vtkm::UInt32 > cntArray){
     cnt = cntArray.PrepareForInPlace(DeviceAdapterTag());
 
   }
+  VTKM_CONT
   void SetVtx(vtkm::cont::ArrayHandle<vtkm::UInt32 > vtxArray){
       vtx = vtxArray.PrepareForInPlace(DeviceAdapterTag());
 
   }
+  VTKM_CONT
   void SetIdx(vtkm::cont::ArrayHandle<vtkm::UInt32 > idxArray){
       idx = idxArray.PrepareForInPlace(DeviceAdapterTag());
 
